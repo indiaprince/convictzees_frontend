b8f1724fbd817aff9dd508b66842194d21216276
indiaprince <hyeonsuhan@gmail.com>
2022-11-08 02:46:27 +0900

    [Bug Fix] background not seen

--- convictzees/src/components/layouts/Layout.tsx
+++ convictzees/src/components/layouts/Layout.tsx
@@ -8,12 +8,17 @@ import Diagram from "../ui/Diagram";
 import RoadMap from "../ui/RoadMap";
 import Section1 from "../ui/Section1";
 import Section2 from "../ui/Section2";
-  
+import Image from 'next/image'
+import background from '../customicons/background.svg';
   
 const Layout = (props : {children: React.ReactNode}) => {
     return (
             <ImageContainer>
-                <Background/>
+                <Image
+                    src={background}
+                    alt="Unable to Load Background"
+                    layout="responsive" 
+                />
                 <Center/>
                 <Header/>
                 <About/>
@@ -31,44 +36,15 @@ const Layout = (props : {children: React.ReactNode}) => {
 
 export default Layout
 
-const Background = styled.div`
-    position : absolute;
-    background-image: url(${"/Background.svg"});
-    background-size: cover;
-
-    background-position:center center;    
-    height: 100%;
-    width: 100%;
-    text-align: center;
-
-`;
-
 const ImageContainer = styled.div`
 
     width: 100%;
     position: relative;
-    min-height: 650vh;
     box-sizing: border-box;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-
-    @media screen and (max-width: 1824px) {
-        min-height: 650vh;
-    }
-    @media screen and (max-width: 1600px) {
-        min-height: 600vh;
-    }
-    @media screen and (max-width: 1440px) {
-        min-height: 550vh;
-    }
-    @media screen and (max-width: 1280px) {
-        min-height: 500vh;
-    }
-    @media screen and (max-width: 1024px) {
-        min-height: 480vh;
-    }
 
 
     `;
