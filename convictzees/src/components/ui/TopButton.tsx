426bb747d57aae7b121685451936084939080ca6
indiaprince <hyeonsuhan@gmail.com>
2022-11-13 00:19:11 +0900

    Fixing commits final

--- convictzees/src/components/ui/TopButton.tsx
+++ convictzees/src/components/ui/TopButton.tsx
@@ -4,14 +4,9 @@ import styled from "styled-components";
 interface IButton {
     title : string;
     fontFamily : string;
-    link : string;
 }
 
 const TopButton =(props : IButton) => {
-    const router = useRouter();
-    const toggle = () => {
-        router.push(props.link);
-    }
     return (
         <TopLogo>
             <LogoText style ={{fontFamily:props.fontFamily}}>
